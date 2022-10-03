7cee8103751d84179efbbcea0f45b1c687629862
luana.fronza <luana.fronza@grad.ufsc.br>
2022-10-03 19:35:30 -0300

    implementa loop para gerar pedidos por minuto

--- src/service/GeradorPedidos.java
+++ src/service/GeradorPedidos.java
@@ -1,8 +1,10 @@
 package service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Properties;
-import java.util.Random;
 
+import org.apache.commons.math3.distribution.GammaDistribution;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,47 +15,53 @@ import serializer.PedidoSerializer;
 
 public class GeradorPedidos {
 
-	private static Random rand = new Random();
-	private static long operacao = 0;
-
 	public static void main(String[] args) throws InterruptedException {
 		Properties properties = new Properties();
 		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, PedidoSerializer.class.getName());
 
+		/*
+		 * Orders were generated each 1 minute, and the number of orders followed a gamma distribution with parameters α = 6.34
+		 * and β = 1.0 (more or less six orders every minute).
+		 */
+		double shape = 6.34;
+		double scale = 1.0;
+
 		try (KafkaProducer<String, Pedido> producer = new KafkaProducer<String, Pedido>(properties)) {
 			while (true) {
 
-				// TODO Adaptar para modelo do George
-				Pedido pedido = gerarPedido();
+				double sample = new GammaDistribution(shape, scale).sample();
 
-				ProducerRecord<String, Pedido> record = new ProducerRecord<String, Pedido>("topico-pedidos", pedido);
-				System.out.println(record.value());
-				producer.send(record);
+				double n_pedidos = round(sample, 0);
+				long tempo_sleep = (long) round((60.0 / n_pedidos), 0);
 
-				// TODO Adaptar para modelo do George
-				// Tempo de chegada dos pedidos 1 a cada 5 segundos
+				System.out.println(n_pedidos + " pedidos = 1 pedido a cada: " + tempo_sleep + " segundos");
 
-				/*
-				 * Orders were generated each 1 minute, and the number of orders followed a gamma distribution with parameters α
-				 * = 6.34 and β = 1.0 (more or less six orders every minute).
-				 */
+				// Cada volta do for representa mais ou menos 60 segundos;
+				for (int i = 0; i <= n_pedidos; i++) {
 
-				// double sample = new GammaDistribution(shape, scale).sample();
+					// get pedido do arquivo
+					Pedido pedido = gerarPedido();
 
-				Thread.sleep(5000);
+					ProducerRecord<String, Pedido> record = new ProducerRecord<String, Pedido>("topico-pedidos", pedido);
+					System.out.println(record.value());
+					producer.send(record);
+
+					Thread.sleep(tempo_sleep * 1000);
+
+				}
 			}
 		}
 	}
 
-	private static Pedido gerarPedido() {
-		Integer n_itens = rand.nextInt(10) + 1;
-		Integer warehouse_block = rand.nextInt(6) + 1;
+	public static double round(double value, int places) {
+		if (places < 0)
+			throw new IllegalArgumentException();
 
-		Pedido pedido = new Pedido(operacao++, n_itens, warehouse_block);
-
-		return pedido;
+		BigDecimal bd = BigDecimal.valueOf(value);
+		bd = bd.setScale(places, RoundingMode.HALF_UP);
+		return bd.doubleValue();
 	}
 
 }
